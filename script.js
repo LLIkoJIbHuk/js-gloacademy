4070d662ecb32ba3e9c68a6b75b0b2d805ecd0fa
LLIkoJIbHuk <kos.y.6094@gmail.com>
2020-01-30 12:10:14 +0300

    commit

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
-if(4*2==8){
-    console.log('text');
-}else {
-    console.log('elsetext');
+let money = prompt('Ваш бюджет?');
+let name =prompt('Название Вашего магазина');
+
+let mainList = {
+    budget: money,
+    shopName: name,
+    shopGoods: [],
+    employers: {},
+    open: false
 }
+
+mainList.shopGoods[0] = promt('Какой тип товара будем продавать?');
+mainList.shopGoods[1] = promt('Какой тип товара будем продавать?');
+mainList.shopGoods[2] = promt('Какой тип товара будем продавать?');
+
+alert(mainList.budget / 30);
+
+console.log(mainList);
