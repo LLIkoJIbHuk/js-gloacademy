7b8cc8ddaa06294bf94a1baa4887ca226e2f6d0a
LLIkoJIbHuk <kos.y.6094@gmail.com>
2020-01-30 12:15:26 +0300

    цикл

--- script.js
+++ script.js
@@ -9,6 +9,11 @@ let mainList = {
     open: false
 }
 
+for(let i = 0; i < 5; i++){
+    let a = prompt('Какой тип товара будем продавать?');
+    mainList.shopGoods[i];
+}
+
 mainList.shopGoods[0] = prompt('Какой тип товара будем продавать?');
 mainList.shopGoods[1] = prompt('Какой тип товара будем продавать?');
 mainList.shopGoods[2] = prompt('Какой тип товара будем продавать?');
