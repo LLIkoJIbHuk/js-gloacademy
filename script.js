31e85d26a641d9b8ae70ba9c521588d74ca781e6
LLIkoJIbHuk <kos.y.6094@gmail.com>
2020-07-23 12:11:17 +0300

    third lesson

--- script.js
+++ script.js
@@ -1,55 +1,56 @@
-/*
+let money, name, time, price;
 
-let money = prompt('Ваш бюджет?');
-let name =prompt('Название Вашего магазина');
-let time = 19;
+function start() {
+  
+  money = prompt("Ваш бюджет?");
+
+  while (isNaN(money) || money == "" || money == null) {
+    money = prompt("Ваш бюджет?");
+  }
+
+  name =prompt("Название вашего магазина?").toUpperCase();
+  time = 21;
+}
+
+start();
 
 let mainList = {
-    budget: money,
-    shopName: name,
-    shopGoods: [],
-    employers: {},
-    open: false
+  budget: money,
+  shopName: name,
+  shopGoods: [],
+  employers: {},
+  open: false, 
+  discount: false
 }
 
-for(let i = 0; i < 5; i++){
+function chooseGoods() {
+  for(let i = 0; i < 5; i++) {
 
     let a = prompt('Какой тип товара будем продавать?');
-
-    if ((typeof(a)) === 'string' && (typeof(a)) === null && a != '' && a.length < 50){
+  
+    if ((typeof(a)) === 'string' && (typeof(a)) != null && a != '' && a.length < 50){
         console.log('Все верно!');
         mainList.shopGoods[i] = a;
-    } else{
-
+    } else {
+      i = i - 1;
     }
+  }
 }
 
-if (time < 0){
+chooseGoods();
+
+function workTime(time) {
+  if (time < 0){
     console.log('Такого не может быть');
-} else if(time > 8 && time < 20){
+  } else if(time > 8 && time < 20){
     console.log('Время работать');
     } else if(time < 24){
-        console.log('Уже слишком поздно!');
-        } else{
-            console.log('В сутках только 24 часа!');
-            } 
-
+      console.log('Уже слишком поздно!');
+      } else{
+        console.log('В сутках только 24 часа!');
+        } 
+}
 
 alert(mainList.budget / 30);
 
 console.log(mainList);
-
-*/
-
-//Callback
-
-function learnJS(lang, callback){
-    console.log('Я учу' + lang);
-    callback();
-} 
-
-learnJS('JS', done);
-
-function done(){
-    console.log('Я прошел 3 урок');
-}
